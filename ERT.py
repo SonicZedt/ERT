31cce4f0884f14232208efde2a6dd58db5ffc770
ZEDT-FOX <zfirht@hotmail.com>
2021-09-21 21:39:08 +0700

    Initial commit

---
+++ ERT.py
@@ -0,0 +1,101 @@
+import os
+import docx
+
+lab = input("Lab: ")
+minggu = int(input("Minggu ke-: "))
+
+#region Data
+days = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"]
+classes = ["2KB", "3KB", "2DC", "3DC"]
+shift = ["Shift 1", "Shift 2", "Shift 3", "Shift 4"]
+assistantName = [
+    "Adinda", "Fauzia", "Sidiq", "Titian",
+    "Zulfikar", "Dicky", "Taruna", "Siti", "Al"
+]
+
+year = "PTA 2021/2022"
+fileLocation = "BAP/Minggu_{n}/".format(n = minggu)
+#file = "{loc}*{type}".format(loc = fileLocation, type = ".docx")
+fileTarget = "Rekap VLab {labName} minggu {n}{type}".format(labName = lab, n = minggu, type = ".txt")
+#endregion
+
+def LoadFile():
+    fileList = []
+    detectedFiles = os.listdir(fileLocation)
+
+    for file in detectedFiles:
+        fileList.append(fileLocation + file)
+    
+    return fileList
+
+def GetFullText(fileIndex):
+    fileList = LoadFile()
+    source = fileList[fileIndex]
+    doc = docx.Document(source)
+    fullText = []
+
+    for paragraph in doc.paragraphs:
+        fullText.append(paragraph.text)
+    
+    return fullText
+
+# Return paragraph that contains str
+def GetParagraph(str, fileIndex):
+    paragraph = GetFullText(fileIndex)
+    for i, j in enumerate(paragraph):
+        if str in j:
+            return(paragraph[i])
+
+def CreateTXT():
+    with open(fileTarget, 'w') as file:
+        file.write("Rekapan PJ Shift {labName} {ta} \n".format(labName = lab, ta = year))
+
+#region Paragraph formatting
+def Date_Shift(fileIndex):
+    date = GetParagraph("Hari", fileIndex)
+    class_shift = GetParagraph("Kelas", fileIndex)
+
+    for day in days:            
+        newdate = date[date.find(day):]
+        if day in date:
+            break
+
+    for c in classes:
+        classIndex = class_shift.find(c)
+        for s in shift:
+            shiftIndex = class_shift.find(s) + len(s) + s.count(" ")
+            if s in class_shift:
+                break
+        newclass = class_shift[classIndex:shiftIndex]
+        if c in class_shift:
+            break
+
+    subheader = "\n{nd} | {nc}".format(nd = newdate, nc = newclass)
+    return subheader
+
+def ListAssistant(fileIndex):
+    presentAssistan = []
+
+    for ass in assistantName:
+        assistant = GetParagraph(ass, fileIndex)
+        if assistant != None:
+            if '*' in assistant:
+                presentAssistan.insert(0,"- " + assistant)
+            else:
+                presentAssistan.append(" - " + assistant)
+    
+    presentAssistanList = '\n'.join(presentAssistan)
+    return presentAssistanList
+#endregion
+
+# Weekly PJS recap into txt
+def RecapTXT():
+    CreateTXT()
+
+    fileList = LoadFile()
+    for fileIndex in range(len(fileList)):
+        formattedParagraph = "{DS} \n {LA} \n\n".format(DS = Date_Shift(fileIndex), LA = ListAssistant(fileIndex))
+        with open(fileTarget, 'a') as fileTXT:
+            fileTXT.write(formattedParagraph)
+    
+    print("Rekapan Lab {labName} minggu {n} selesai dibuat!".format(labName = lab, n = minggu))
