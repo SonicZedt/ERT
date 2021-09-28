5914a8c6c1feaecba85b1784eed260ea7af26860
ZEDT-FOX <zfirht@hotmail.com>
2021-09-28 17:18:58 +0700

    add teminate delay

--- Script/ERT.py
+++ Script/ERT.py
@@ -4,6 +4,12 @@ def SetupUI():
     UI.SetTextColor()
     UI.Header()    
 
+def TerminateIn(delay):
+    import time
+    for d in range(delay):
+        print("Keluar otomatis dalam", 5-d)
+        time.sleep(1)
+
 def main():
     SetupUI()
 
@@ -12,6 +18,7 @@ def main():
 
     Recap.RecapTXT()
     Recap.RecapDOCX()
+    TerminateIn(5)
 
 if __name__ == "__main__":
     main()
