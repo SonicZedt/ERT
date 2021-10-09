c96079513a2c53da77a558c74e5218889f58cd61
ZEDT-FOX <zfirht@hotmail.com>
2021-10-09 23:17:40 +0700

    Data modifier

---
+++ Script/Data.py
@@ -0,0 +1,17 @@
+import pickle
+import requests
+
+var = ''
+url = ''
+
+def GetOnline():
+    requests.get(url, allow_redirects=True)
+    return url.rsplit('/', 1)[1]
+
+def Create(fileName):
+    with open(fileName, 'wb') as file:
+        pickle.dump(var, file)
+
+def Read(fileName):
+    with open(fileName, 'rb') as file:
+        print(pickle.load(file))
